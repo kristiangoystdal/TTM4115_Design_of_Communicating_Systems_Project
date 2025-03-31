62596a25222e735760973566c14440afd72b5876
Kristian Goystdal <krisgoy100@gmail.com>
2025-03-31 23:06:06 +0200

    feat: Update asset references in index.html and enhance NavBar component with descriptive labels

--- Code/mobile/templates/scooter-app/src/components/NavBar.vue
+++ Code/mobile/templates/scooter-app/src/components/NavBar.vue
@@ -1,24 +1,24 @@
 <template>
   <v-bottom-navigation app color="primary" grow height="56" class="bottom-navbar">
     <v-btn to="/" icon>
-      <v-icon>mdi-home</v-icon>
+      <v-icon>mdi-home</v-icon> Home
     </v-btn>
 
     <template v-if="user?.username">
       <v-btn to="/bookings" icon>
-        <v-icon>mdi-calendar-check</v-icon>
+        <v-icon>mdi-calendar-check</v-icon> Booking History
       </v-btn>
       <v-btn icon @click="logout">
-        <v-icon>mdi-logout</v-icon>
+        <v-icon>mdi-logout</v-icon> Logout
       </v-btn>
     </template>
 
     <template v-else>
       <v-btn to="/login" icon>
-        <v-icon>mdi-login</v-icon>
+        <v-icon>mdi-login</v-icon> Login
       </v-btn>
       <v-btn to="/register" icon>
-        <v-icon>mdi-account-plus</v-icon>
+        <v-icon>mdi-account-plus</v-icon> Register
       </v-btn>
     </template>
   </v-bottom-navigation>
