41996c245cd9c8d013b9e54ca6bfb49e228b00d9
Kristian Gøystdal <krisgoy100@gmail.com>
2025-04-03 17:03:22 +0200

    fix: Update lights_reserved method to use Color.RED for LED matrix

--- Code/scooter/scooter_logic.py
+++ Code/scooter/scooter_logic.py
@@ -7,6 +7,8 @@ from scooter.helpers import IdCounter
 
 from .sense_hat_handler import set_led_matrix
 
+from scooter.color import Color
+
 
 @dataclass(slots=True)
 class ScooterLogic:
@@ -14,11 +16,7 @@ class ScooterLogic:
     mqtt_client: Client = field(default=None, init=False)  # type: ignore
     stm: Machine = field(default=None, init=False)
 
-    # Define colors as class variables
-    IDLE_COLOR = (0, 255, 0)
-    RESERVED_COLOR = (255, 255, 0)
-    DRIVING_COLOR = (0, 0, 255)
-    CHARGING_COLOR = (255, 0, 0)
+    
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.scooter_id})"
@@ -26,7 +24,7 @@ class ScooterLogic:
     def lights_reserved(self) -> None:
         """Set the LED matrix to reserved color."""
         print("Entering reserved state: Setting LED matrix to reserved color.")
-        set_led_matrix(self.RESERVED_COLOR)
+        set_led_matrix(Color.RED)
 
     def print_reserved(self):
         print(">>> ENTERED reserved state!")
