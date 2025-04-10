e5e41de46c010dec6de88bd8f2e5cad2cf70ad77
Kristian Gøystdal <krisgoy100@gmail.com>
2025-04-10 15:35:14 +0200

    feat: Add transition from charging to driving on unlock trigger in state machine

--- Code/scooter_app.py
+++ Code/scooter_app.py
@@ -68,7 +68,12 @@ t9 = {
     "target": "idle",
     "trigger": "no_charge",
 }
-transitions = [t1, t2, t3, t4, t5, t6, t7, t8, t9]
+t10 = {
+    "source": "charging",
+    "target": "driving",
+    "trigger": "unlock",
+}
+transitions = [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10]
 
 
 def main() -> None:
